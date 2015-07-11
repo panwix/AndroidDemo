e69535ad99ad8f40483909138a65538da8fd9475
Panwix <1369834653@qq.com>
2015-07-11 16:24:26 +0800

    2015/07/09

---
+++ app/src/main/java/com/panwix/androiddemo/widget/LabelEditText.java
@@ -0,0 +1,67 @@
+package com.panwix.androiddemo.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.panwix.androiddemo.R;
+
+/**
+ * Created by Panwix on 15/7/11.
+ */
+public class LabelEditText extends LinearLayout {
+    private TextView textView;
+    private String labelText;
+    private int labelFontSize;
+    private String labelPosition;
+
+    public LabelEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //读取labelText属性值
+        int resourceId = attrs.getAttributeResourceValue(null, "labelText", 0);
+
+        //resourceId为0，可能时字符串
+        if(resourceId == 0)
+            labelText = attrs.getAttributeValue(null, "labelText");
+        else
+            labelText = getResources().getString(resourceId);
+
+        //未取到labelText属性值，则抛出异常
+        if(labelText == null)
+            throw new RuntimeException("必须设置labelText属性值");
+
+        //获取labelFontSize属性值
+        resourceId = attrs.getAttributeResourceValue(null, "labelFontSize", 0);
+        if(resourceId == 0)
+            labelFontSize = attrs.getAttributeIntValue(null, "labelFontSize", 14);
+        else
+            labelFontSize = getResources().getInteger(resourceId);
+
+        //获取labelPosition属性值
+        resourceId = attrs.getAttributeResourceValue(null, "labelPosition", 0);
+        if(resourceId == 0)
+            labelPosition = attrs.getAttributeValue(null, "labelPosition");
+        else
+            labelPosition = getResources().getString(resourceId);
+        if(labelPosition == null)
+            labelPosition = "left";
+
+        //获得LAYOUT_INFLATER_SERVIC服务
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li = (LayoutInflater) context.getSystemService(infService);
+
+        if("left".equals(labelPosition))
+            li.inflate(R.layout.labeledittext_horizontal, this);
+        else if("top".equals(labelPosition))
+            li.inflate(R.layout.labeledittext_vertical, this);
+        else
+            throw new RuntimeException("labelPosition属性只有left或top");
+
+        textView = (TextView)findViewById(R.id.textview);
+        textView.setTextSize((float)labelFontSize);
+        textView.setText(labelText);
+    }
+}
